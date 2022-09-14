41d8c8b22df0952f523410a7c5e6a7f8f634d8fe
taichi.murai <detimilnutm@outlook.jp>
2022-09-14 08:58:11 +0000

    add kadai-fizzbuzz.rb

--- kadai-fizzbuzz.rb
+++ kadai-fizzbuzz.rb
@@ -1,15 +1,17 @@
-num_max = 100
-(1..num_max).each do |num|
-  def fizzbuzz(num)
-    if num % 3 == 0
-      puts "Fizz"
-    elsif num % 5 == 0
-      puts "Buzz"
-    elsif num % 3 == 0 && num % 5 == 0
-      puts "FizzBuzz"
-    else
-      puts num
-    end
+def fizzbuzz(num)
+  if num % 15 == 0
+    return "FizzBuzz"
+  elsif num % 3 == 0
+    return "Fizz"
+  elsif num % 5 == 0
+    return "Buzz"
+  else 
+    return num.to_s
   end
+end
+
+
+num_max = 100
+(1..100).each do |num|
   puts fizzbuzz(num)
 end
